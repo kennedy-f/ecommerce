1b0447363928af9009f4dbe18acfdbed0dfc3189
Natan Meira <natan.libra@hotmail.com>
2020-06-06 22:37:30 -0300

    Telas do site

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('admin/pages/dashboard');
+// });
 Route::get('/', function () {
-    return view('admin/pages/dashboard');
+    return view('site/layouts/app');
 });
 
 Auth::routes();
